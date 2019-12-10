1b1f60828cfd57b0d882700b395eb140d0515113
Silva <walme004@gmail.com>
2019-12-10 18:11:57 -0500

    adding async await

--- js/async-await.js
+++ js/async-await.js
@@ -4,15 +4,41 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Handle all fetch requests
+async function getJSON(url){
+  try{
+       const response =  await fetch(url)
+       return await response.json();
+  } catch(error) {
+       throw error;
+  }
+}
 
+async function getPeopleInSpace(){
+
+   const peopleJson = await getJSON(astrosUrl)
+   
+   const profiles = peopleJson.people.map( async (person) => {
+     const craft = person.craft;
+     const profileJson = await getJSON(wikiUrl + person.name);
+
+     return { ...profileJson, craft };
+   });
+
+   return Promise.all(profiles);
+}
 
 // Generate the markup for each profile
 function generateHTML(data) {
+  console.log(data);
   data.map( person => {
     const section = document.createElement('section');
     peopleList.appendChild(section);
+    var source;
+    if(person.thumbnail){
+      source = person.thumbnail.source;
+    }
     section.innerHTML = `
-      <img src=${person.thumbnail.source}>
+      <img src=${source}>
       <span>${person.craft}</span>
       <h2>${person.title}</h2>
       <p>${person.description}</p>
@@ -24,4 +50,11 @@ function generateHTML(data) {
 btn.addEventListener('click', (event) => {
   event.target.textContent = "Loading...";
 
+  getPeopleInSpace()
+  .then(generateHTML)
+  .catch( err => {
+    peopleList.innerHTML = '<h3>Something went wrong!</h3>';
+    console.log(err)
+  })
+  .finally( () =>   event.target.remove());
 });
